7119ad2a779421457c9ca8006f957c5615dca0af
Ariouz <Ariouz@users.noreply.github.com>
2026-02-01 01:40:15 +0100

    Added more build args

--- src/main/java/fr/ariouz/gkit/build/image/arg/NativeBuildArg.java
+++ src/main/java/fr/ariouz/gkit/build/image/arg/NativeBuildArg.java
@@ -1,8 +1,6 @@
 package fr.ariouz.gkit.build.image.arg;
 
-import fr.ariouz.gkit.build.image.arg.renderer.BooleanFlagRenderer;
-import fr.ariouz.gkit.build.image.arg.renderer.KeyValueListFlagRenderer;
-import fr.ariouz.gkit.build.image.arg.renderer.NativeBuildArgRenderer;
+import fr.ariouz.gkit.build.image.arg.renderer.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,9 +16,77 @@ public enum NativeBuildArg {
 			"initializeAtBuildTime",
 			"initialize-at-build-time",
 			new KeyValueListFlagRenderer()
-	);
+	),
+	INITIALIZE_AT_RUN_TIME(
+			"initializeAtRunTime",
+			"initialize-at-run-time",
+			new KeyValueListFlagRenderer()
+	),
+	SHARED_LIBRARY(
+			"buildSharedLibrary",
+			"shared",
+			new BooleanFlagRenderer()
+	),
+	SILENT_OUTPUT(
+			"silentBuild",
+			"silent",
+			new BooleanFlagRenderer()
+	),
+	VERBOSE_OUTPUT(
+			"verboseBuild",
+			"verbose",
+			new BooleanFlagRenderer()
+	),
+	GEN_DEBUG_INFO(
+			"debugInfo",
+			"g",
+			new CompactBooleanFlagRenderer()
+	),
 
-	// TODO AdditionalArgsListFlagRenderer: List<String>
+	ENABLE_HTTP(
+			"enableHttp",
+			"enable-http",
+			new BooleanFlagRenderer()
+	),
+	ENABLE_HTTPS(
+			"enableHttps",
+			"enable-https",
+			new BooleanFlagRenderer()
+	),
+	ENABLE_ALL_SECURITY_SERVICES(
+			"enableAllSecurityServices",
+			"enable-all-security-services",
+			new BooleanFlagRenderer()
+	),
+	REPORT_UNSUPPORTED_AT_RUNTIME(
+			"reportUnsupportedAtRuntime",
+			"report-unsupported-elements-at-runtime",
+			new BooleanFlagRenderer()
+	),
+	ALLOW_INCOMPLETE_CLASSPATH(
+			"allowIncompleteClasspath",
+			"allow-incomplete-classpath",
+			new BooleanFlagRenderer()
+	),
+	LIBC(
+			"libc",
+			"libc",
+			new KeyValueFlagRenderer()
+	),
+	OPTIMIZATION_LEVEL(
+			"optimizationLevel",
+			"O",
+			new CompactKeyValueFlagRenderer()
+	),
+
+	RAW_ARGS(
+			"rawArgs",
+			"",
+			new RawArgsRenderer()
+	),
+
+
+	;
 
 
 	private final String configKey;
@@ -51,7 +117,10 @@ public enum NativeBuildArg {
 		return Arrays.stream(values())
 				.filter(a -> a.configKey.equals(key))
 				.findFirst()
-				.orElseThrow(() -> new BuildArgException("Unknown native image build arg: " + key) );
+				.orElseThrow(() -> new BuildArgException("Unknown native image build arg: '" + key+"'") );
 	}
 
+	public NativeBuildArgRenderer<?> getRenderer() {
+		return renderer;
+	}
 }
